1b8290e2384786a029634f72217c2cdec6df12d4
Mateusz Baucz <mateuszbaucz@gmail.com>
2022-07-27 15:36:59 +0200

    lekkie poprawki

--- provision.sh
+++ provision.sh
@@ -17,3 +17,5 @@ EOF
 mysql -u root -pT7zGwUrURNNk35%N -e "show databases;"
 mysql -u root -pT7zGwUrURNNk35%N -e "CREATE USER 'aplikacja_back'@'%' IDENTIFIED BY 'J234re5GT4s2agAv';"
 mysql -u root -pT7zGwUrURNNk35%N -e "CREATE USER 'aplikacja_front'@'%' IDENTIFIED BY 'nTwqXK2AS34nGg8X';"
+sed -i -e 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+systemctl restart mysql
